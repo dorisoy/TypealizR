d0bb8b975da73b6aa7759ce358a2e16128aa0cfd
earloc <earloc@live.de>
2023-01-17 22:59:29 +0100

    add poc base

---
+++ src/Playground.Xliff.Console/XliffStringLocalizer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+
+namespace Playground.Xliff.Console;
+internal class XliffStringLocalizer : IStringLocalizer
+{
+    public LocalizedString this[string name] => throw new NotImplementedException();
+
+    public LocalizedString this[string name, params object[] arguments] => throw new NotImplementedException();
+
+    public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures)
+    {
+        throw new NotImplementedException();
+    }
+}
